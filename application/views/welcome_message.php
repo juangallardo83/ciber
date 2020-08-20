f8005ed031edab1d3423a72982ca8a52984330b3
Juan Carlos Gallardo <j.gallardovasquez@uandresbello.edu>
2020-08-20 00:40:15 -0400

    Manual usario y barra inicio

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,7 +8,60 @@
         <link href="<?php echo base_url(); ?>public/css/style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
+    
+        
+<nav class="navbar nav-color">
+    <span class="navbar-brand mb-0 h1 fuente">
+        <img src="<?php echo base_url(); ?>public/images/logo.png" width="48px" >    CIBERMADUREZ
+    </span>
+    
+<div class="card-footer madurez "> 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              
+                <li class="nav-item">
+                    <a class="nav-link" href="Welcome" data-toggle="modal" data-target="#modalmanual"> <i class="fa fa-book" aria-hidden="true"></i> Manual de Usuario</a>
+                </li>  
+                               
+
+        </div>
+        
+        
+                <div class="modal fade" id="modalmanual" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document" style="max-width: 850px;;margin-top: 150px;">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h6 class="modal-title" id="Modalmanual">Manual de Usuario  </h6>
+
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body" id="itemsub">
+                     <   <iframe src="<?php echo base_url(); ?>public/pdf/manual.pdf"                              
+                                style="width:830px; height:600px;" frameborder="0"></iframe>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+</div>
+    
+    
+    
+    
+    
+ 
+</nav>
         <!--<?php require_once 'component/navbar.php'; ?>-->
 
         <div class="container">
